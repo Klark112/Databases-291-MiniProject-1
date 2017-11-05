f6569ff2cb61fc3baef998dbc1260c40f4ab600c
Justin Daza <justinjo@ualberta.ca>
2017-11-05 11:09:16 -0700

    Ordered search query implemented

--- mp1-search.py
+++ mp1-search.py
@@ -14,7 +14,37 @@ from collections import defaultdict
 #The input I use to test this function is: 4l milk
 #
 import sqlite3
+
+
+from itertools import chain
 DATABASE = 'mp1.db'
+
+"""
+SAMPLE SEARCH QUERY if input 'milk', '4', 'cat' --> tested and works
+
+SELECT pname , sname, count(*) as matches
+FROM(
+    SELECT pr.name as pname, st.name as sname
+    FROM products pr 
+    INNER JOIN carries ca ON pr.pid = ca.pid 
+    INNER JOIN stores st  ON ca.sid = st.sid 
+    WHERE pr.name LIKE ('%' ||'milk'|| '%')
+    UNION ALL
+    SELECT pr.name as pname, st.name as snams
+    FROM products pr 
+    INNER JOIN carries ca ON pr.pid = ca.pid 
+    INNER JOIN stores st  ON ca.sid = st.sid 
+    WHERE pr.name LIKE ('%' ||'4'|| '%')
+    UNION ALL
+    SELECT pr.name as pname, st.name as sname
+    FROM products pr 
+    INNER JOIN carries ca ON pr.pid = ca.pid 
+    INNER JOIN stores st  ON ca.sid = st.sid 
+    WHERE pr.name LIKE ('%' ||'cat'|| '%')
+    )
+GROUP BY pname, sname
+ORDER BY matches DESC
+"""
 
 def search_Items(search_terms):
     conn = sqlite3.connect(DATABASE)
@@ -23,12 +53,39 @@ def search_Items(search_terms):
     #get search terms
 
     search_terms_list=search_terms.split()
-    term_matches={}
+    term_matches=[]
     print(search_terms_list)
+    query = 'SELECT pname , sname, count(*) as matches FROM('
+    for key in search_terms_list:
+        print(key)
+        query +=" SELECT pr.name as pname, st.name as sname FROM products pr INNER JOIN carries ca ON pr.pid = ca.pid INNER JOIN stores st  ON ca.sid = st.sid WHERE pr.name LIKE ('%' ||'" + key + "'|| '%') UNION ALL"
 
-    for i in search_terms_list:
+    query = query[:-9] + ')GROUP BY pname, sname ORDER BY  matches DESC, sname DESC'
+    print(query)
+    c.execute(query)
+    res = c.fetchall()
+    for i in res:
         print(i)
+    conn.commit()
+    conn.close()
+"""
+        c.execute('''
+           SELECT pr.name, st.name
+           FROM products pr, stores st, carries ca
+           WHERE pr.name LIKE ('%' || ? || '%') and 
+           pr.pid=ca.pid and 
+           st.sid=ca.sid
+           ''', (key,))
+        res = c.fetchall()
+        print(res)
+        for i in res:
+            sub_res = [i,1]
+            if i in chain.from_iterable(term_matches):
+                term_matches.append(sub_res)
+            else:
+                term_matches[term_matches.index(i)][1] += 1
 
+"""
 
 
 """
@@ -48,8 +105,6 @@ def search_Items(search_terms):
     found_terms.sort()
 """
 
-    conn.commit()
-    conn.close()
 
 
 if __name__ == "__main__":
